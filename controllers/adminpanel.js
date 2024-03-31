4944de00f9b9d83492d1157c219c4589ea4ea9cb
smit trivedi <smitt533@gmail.com>
2024-03-31 13:17:57 +0530

    check readme.md file

--- controllers/adminpanel.js
+++ controllers/adminpanel.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 class AdminPanel {
     static getAllUser = (req, res) => {
+        // we have set file path of admin-dashboard.html !
         const filePath = path.join(process.cwd(), 'dist','aprycot','html','dashboard','dist','dashboard','admin-dashboard.html');
     res.sendFile(filePath, (err) => {
         if (err) {
+            // if path will not match it will render error!
             console.error('Error sending file:', err);
             res.status(500).send('Internal Server Error');
         } else {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,12 +6,15 @@ const {getUserCount} = require('../controllers/AdminController');
 const AdminPanel = require('../controllers/adminpanel')
 const router = express.Router();
 
-router.route('/register').post(register)
-router.route('/login').post(login)
+router.route('/register').post(register);
+router.route('/login').post(login);
+// here we have issue with unAuthenticated issue with middleware 
+// bcz of jwt related issue!
 router.get('/admin/userCount', authenticateAdmin, getUserCount);
 
 
-
+// here we servering Html File =>  admin-panen.html !
+// just need to adjust css issue
 router.get('/admin/get/panel',AdminPanel.getAllUser);
 
 module.exports =  router;
