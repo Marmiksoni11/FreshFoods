66ce8d597410dee514c226838e1ab9923e3f6cb2
smit trivedi <smitt533@gmail.com>
2024-04-24 21:43:14 +0530

    first Commit

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
+
 class BadRequestError extends CustomAPIError {
   constructor(message) {
     super(message);
