27fd7473494e201a67063374dde909604878fd5c
smit trivedi <smitt533@gmail.com>
2024-04-26 13:56:12 +0530

    profile.html backend set just  front end design later

--- dist/hamburger.js
+++ dist/hamburger.js
@@ -46,3 +46,59 @@ menuToggle.onclick = function () {
   } else {
       console.error('Authentication token not found in local storage');
   }
+
+
+
+
+  
+
+
+// Check if the token exists
+
+if(authToken) {
+    // Fetch user details using the authentication token
+    fetch('http://localhost:4008/api/v1/user', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${authToken}`, // Include the authentication token in the headers
+            'Content-Type': 'application/json',
+            
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Failed to fetch user details');
+    })
+    .then(data => {
+        console.log("User Details profile.js:",data,"FRom Profile.js ");
+
+            console.log(data.coverImage,"https:");
+        // console.log('User details:', data.coverImage);
+        // console.log('User details:', data.avatar);
+
+        
+        // Update profile information
+        document.querySelector(".font-semibold").textContent = data.username;
+        document.querySelector(".useremail").textContent = data.email;
+        
+
+        // Update cover image
+        document.querySelector(".cover-image").src = data.coverImage;
+        
+        // Update Avatar image
+        
+        document.querySelector(".avatar-image").src = data.avatar;
+
+        // // Update full name
+        // document.querySelector(".userfullname").textContent = data.fullname;
+    })
+    .catch(error => {
+        console.error('Error fetching user details:', error);
+    });
+} else {
+    console.error('Authentication token not found in local storage');
+}
+
+
--- dist/profile.js
+++ dist/profile.js
@@ -21,7 +21,7 @@ if(authToken) {
         throw new Error('Failed to fetch user details');
     })
     .then(data => {
-        console.log(data);
+        console.log("User Details profile.js:",data,"FRom Profile.js ");
 
             console.log(data.coverImage,"https:");
         // console.log('User details:', data.coverImage);
@@ -47,68 +47,3 @@ if(authToken) {
 }
 
 
-
-if (authToken) {
-    // Fetch user details using the authentication token
-    fetch('http://localhost:4008/api/v1/profile/update-avatar', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${authToken}`, // Include the authentication token in the headers
-            'Content-Type': 'application/json',
-        }
-    })
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error('Failed to fetch user details');
-    })
-    .then(data => {
-        // console.log('User details:', data.avatar);
-        console.log(data,"got Avatar");
-
-        // Construct the URL for the avatar image
-        
-        // console.log(avatarUrl,"kjsfkjhsdkjfhsdkhsdkj");
-        // Update avatar image
-        document.querySelector(".avatar-image").src = data.avatar;
-    })
-    .catch(error => {
-        console.error('Error fetching user details:', error);
-    });
-} else {
-    console.error('Authentication token not found in local storage');
-}
-
-// COVER IMAGE
-if (authToken) {
-    // Fetch user details using the authentication token
-    fetch('http://localhost:4008/api/v1/profile/update-cover-image', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${authToken}`,
-            'Content-Type': 'application/json',
-            // Include the authentication token in the headers
-        }
-    })
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error('Failed to fetch user details');
-    })
-    .then(data => {
-        // console.log('User details:', data.avatar);
-        console.log(data,"got CoverImage from profile.js");
-        // Construct the URL for the avatar image
-       
-        // console.log(avatarUrl,"kjsfkjhsdkjfhsdkhsdkj");
-        // Update avatar image
-        document.querySelector(".avatar-image").src = data.avatar ;
-    })
-    .catch(error => {
-        console.error('Error fetching user details:', error);
-    });
-} else {
-    console.error('Authentication token not found in local storage');
-}
